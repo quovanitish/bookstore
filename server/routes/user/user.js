9a7cf1c7b310c8fc15df335cbe70d827b9ac4055
quovanitish <nitish.sharma1@quovantis.com>
2021-11-22 10:44:16 +0530

    Logout and get user implemented in user routers

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../core/middleware/auth/auth");
 const User = require("../../core/db/models/user/user");
 
 router.post("/users", async (req, res) => {
@@ -24,6 +25,24 @@ router.post("/users/login", async (req, res) => {
     res.send({ user, token });
   } catch (err) {
     res.status(400).send({ error: err.message });
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token === req.token;
+    });
+
+    console.log(req.user.tokens);
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send();
   }
 });
 
