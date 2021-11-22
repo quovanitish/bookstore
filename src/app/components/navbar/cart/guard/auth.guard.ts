e3bd4d36a4d38ab904085128d7e494a83198fb5d
quovanitish <nitish.sharma1@quovantis.com>
2021-11-23 00:45:27 +0530

    Navbar component with login, logout, signup and a cart component

---
+++ src/app/components/navbar/cart/guard/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate([environment.ui.login]);
+      return false;
+    } else {
+      return this.authService.isLoggedIn();
+    }
+  }
+}
