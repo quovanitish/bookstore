b5728df02c59ab82615e479a8f3cf7b6f582f99e
quovanitish <nitish.sharma1@quovantis.com>
2021-11-23 01:52:34 +0530

    Fethed all the books in cart from DB and handled the click event of book component

--- src/app/components/navbar/cart/cart.component.ts
+++ src/app/components/navbar/cart/cart.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { BookstoreService } from 'src/app/services/bookstore/bookstore.service';
+import { Book } from 'src/models/book/book';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  books: Book[] = [];
+  action: string = 'Remove from cart';
 
-  constructor() { }
+  constructor(
+    private bookService: BookstoreService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.bookService
+      .getCart()
+      .then((res) => {
+        this.books = res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
+  handleRemoveFromCart = (bookId: string): void => {
+    this.books = this.books.filter((bookObject) => {
+      return bookId !== bookObject.id;
+    });
+    
+    if (!this.authService.isLoggedIn()) {
+      alert('Please log in first to add a book to the cart');
+      return;
+    } else {
+      this.bookService.removeFromCart(bookId).subscribe(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  };
 }
