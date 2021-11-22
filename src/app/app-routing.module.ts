b6d59723e9c0e14a1f3a83dc05ef02ede277c67d
quovanitish <nitish.sharma1@quovantis.com>
2021-11-23 00:51:27 +0530

    Implemented routing for UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookstoreComponent } from './components/bookstore/bookstore.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/navbar/cart/cart.component';
+import { AuthGuard } from './components/navbar/cart/guard/auth.guard';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
+  { path: '', component: BookstoreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
