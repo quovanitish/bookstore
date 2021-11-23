70a7964f658308347f652e2c9044d929e0d1eda5
quovanitish <nitish.sharma1@quovantis.com>
2021-11-23 12:12:33 +0530

    Implemented TDF form for login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { environment } from 'src/environments/environment';
+import { Login } from 'src/models/login/login';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,24 @@ import { environment } from 'src/environments/environment';
 export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
+  user = new Login('', '');
+
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.router.navigate([environment.ui.bookstore]);
     }
+  }
+
+  // event handler for form submit
+  onSubmit(userLoginObj: Login) {
+    this.authService.login(userLoginObj).subscribe(
+      (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate([environment.ui.bookstore]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
