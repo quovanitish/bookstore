f82e9313e7f513cb0b53b223ed1d2f9e9b90ab28
quovanitish <nitish.sharma1@quovantis.com>
2021-11-28 21:54:17 +0530

    Implemented navbar view for throughout the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './services/auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hello from bookstore';
+  constructor(private router: Router, public authService: AuthService) {}
+
+  handleCartClick = (): void => {
+    this.router.navigate([environment.ui.cart]);
+  };
+
+  handleLoginClick = (): void => {
+    this.router.navigate([environment.ui.login]);
+  };
+
+  handleSignUpClick = (): void => {
+    this.router.navigate([environment.ui.signup]);
+  };
+
+  handleLogOutClick = (): void => {
+    this.authService.logout().subscribe(
+      (response) => {
+        console.log('Logout Success');
+        localStorage.removeItem('token');
+        this.router.navigate([environment.ui.bookstore]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
 }
