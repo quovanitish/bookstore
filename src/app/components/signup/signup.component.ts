f5478f4ff72d233d92ee099badc11b5c6de5a6ce
quovanitish <nitish.sharma1@quovantis.com>
2021-11-28 21:50:03 +0530

    Implemented signup component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { environment } from 'src/environments/environment';
+import { Signup } from 'src/models/signup/signup';
 
 @Component({
   selector: 'app-signup',
@@ -11,9 +12,15 @@ import { environment } from 'src/environments/environment';
 export class SignupComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
 
+  newUser = new Signup('', '', '');
+
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.router.navigate([environment.ui.bookstore]);
     }
   }
+
+  onSubmit = (signupObject: SignupComponent): void => {
+    console.log(signupObject);
+  };
 }
